39d24ea26012555182c26aabea3e261ef187d6c9
vpraion <vpraion@proton.me>
2026-07-06 10:20:34 +0200

    added directory mappings for resource management functions

--- internal/db/db.go
+++ internal/db/db.go
@@ -9,13 +9,68 @@ import (
 	"strings"
 )
 
+type ResourceDir string
+
+const (
+	DirAbility                 ResourceDir = "ability"
+	DirBerry                   ResourceDir = "berry"
+	DirBerryFirmness           ResourceDir = "berry-firmness"
+	DirBerryFlavor             ResourceDir = "berry-flavor"
+	DirCharacteristic          ResourceDir = "characteristic"
+	DirContestEffect           ResourceDir = "contest-effect"
+	DirContestType             ResourceDir = "contest-type"
+	DirEggGroup                ResourceDir = "egg-group"
+	DirEncounterCondition      ResourceDir = "encounter-condition"
+	DirEncounterConditionValue ResourceDir = "encounter-condition-value"
+	DirEncounterMethod         ResourceDir = "encounter-method"
+	DirEvolutionChain          ResourceDir = "evolution-chain"
+	DirEvolutionTrigger        ResourceDir = "evolution-trigger"
+	DirGender                  ResourceDir = "gender"
+	DirGeneration              ResourceDir = "generation"
+	DirGrowthRate              ResourceDir = "growth-rate"
+	DirItem                    ResourceDir = "item"
+	DirItemAttribute           ResourceDir = "item-attribute"
+	DirItemCategory            ResourceDir = "item-category"
+	DirItemFlingEffect         ResourceDir = "item-fling-effect"
+	DirItemPocket              ResourceDir = "item-pocket"
+	DirLanguage                ResourceDir = "language"
+	DirLocation                ResourceDir = "location"
+	DirLocationArea            ResourceDir = "location-area"
+	DirMachine                 ResourceDir = "machine"
+	DirMove                    ResourceDir = "move"
+	DirMoveAilment             ResourceDir = "move-ailment"
+	DirMoveBattleStyle         ResourceDir = "move-battle-style"
+	DirMoveCategory            ResourceDir = "move-category"
+	DirMoveDamageClass         ResourceDir = "move-damage-class"
+	DirMoveLearnMethod         ResourceDir = "move-learn-method"
+	DirMoveTarget              ResourceDir = "move-target"
+	DirNature                  ResourceDir = "nature"
+	DirPalParkArea             ResourceDir = "pal-park-area"
+	DirPokeathlonStat          ResourceDir = "pokeathlon-stat"
+	DirPokedex                 ResourceDir = "pokedex"
+	DirPokemon                 ResourceDir = "pokemon"
+	DirPokemonColor            ResourceDir = "pokemon-color"
+	DirPokemonForm             ResourceDir = "pokemon-form"
+	DirPokemonHabitat          ResourceDir = "pokemon-habitat"
+	DirPokemonShape            ResourceDir = "pokemon-shape"
+	DirPokemonSpecies          ResourceDir = "pokemon-species"
+	DirRegion                  ResourceDir = "region"
+	DirStat                    ResourceDir = "stat"
+	DirSuperContestEffect      ResourceDir = "super-contest-effect"
+	DirType                    ResourceDir = "type"
+	DirVersion                 ResourceDir = "version"
+	DirVersionGroup            ResourceDir = "version-group"
+)
+
 var (
 	dataDir = "data"
 	cache   = make(map[string]any)
 )
 
 // Fetches resource by id as long as p_resourceType, id are valid
-func GetById[T any](p_resourceType string, id int) (*T, error) {
+//
+// Usage showcase : db.GetById[models.Pokemon](db.DirPokemon, 6)
+func GetById[T any](p_resourceType ResourceDir, id int) (*T, error) {
 	idStr := strconv.Itoa(id)
 	cacheKey := fmt.Sprintf("%s/%s", p_resourceType, idStr)
 
@@ -25,7 +80,7 @@ func GetById[T any](p_resourceType string, id int) (*T, error) {
 	}
 
 	// If not, load the JSON file
-	filePath := filepath.Join(dataDir, p_resourceType, idStr, idStr+".json")
+	filePath := filepath.Join(dataDir, string(p_resourceType), idStr, idStr+".json")
 	data, err := os.ReadFile(filePath)
 	if err != nil {
 		return nil, fmt.Errorf("resource %s (ID: %d) not found : %w", p_resourceType, id, err)
@@ -41,7 +96,9 @@ func GetById[T any](p_resourceType string, id int) (*T, error) {
 }
 
 // Fetches the resource list as long as p_resourceType is valid
-func GetAll[T any](p_resourceType string) ([]*T, error) {
+//
+// Usage showcase : db.GetAll[models.Pokemon](db.DirPokemon)
+func GetAll[T any](p_resourceType ResourceDir) ([]*T, error) {
 	cacheKey := fmt.Sprintf("%s/all", p_resourceType)
 
 	// Check if the list is already cached
@@ -49,7 +106,7 @@ func GetAll[T any](p_resourceType string) ([]*T, error) {
 		return val.([]*T), nil
 	}
 
-	pattern := filepath.Join(dataDir, p_resourceType, "*", "*.json")
+	pattern := filepath.Join(dataDir, string(p_resourceType), "*", "*.json")
 	files, err := filepath.Glob(pattern)
 	if err != nil {
 		return nil, err
