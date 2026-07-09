55cfde50322894fd54c7ef7a202ecd4756646728
vpraion <vpraion@proton.me>
2026-07-09 09:13:01 +0200

    Implement HTTP server with configurable port and add GitHub Pages deployment workflow

--- main.go
+++ main.go
@@ -1,20 +1,28 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net/http"
-	"os"
 	"pokegrep/internal/builder"
 )
 
 func main() {
+	// Env variables parsing
+	serveFlag := flag.Bool("serve", false, "Démarrer un serveur HTTP de prévisualisation local")
+	portFlag := flag.String("port", "8080", "Port d'écoute du serveur de prévisualisation")
+	flag.Parse()
+
 	fmt.Println("Building...")
 	builder.Build()
-	fs := http.FileServer(http.Dir("dist"))
-	fmt.Println("Build Done! Server running at http://localhost:8081")
-	err := http.ListenAndServe(":8081", fs)
-	if err != nil {
-		panic(err)
+	fmt.Println("Build complete!")
+
+	if *serveFlag {
+		fs := http.FileServer(http.Dir("dist"))
+		fmt.Println("Server running at http://localhost:8080")
+		err := http.ListenAndServe(":"+*portFlag, fs)
+		if err != nil {
+			panic(err)
+		}
 	}
-	os.Exit(0)
 }
